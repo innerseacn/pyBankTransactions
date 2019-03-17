b005146b3c12eca9c818dfcf66b5de70cb04cd0e
innerseacn <42731705+innerseacn@users.noreply.github.com>
2019-03-17 22:44:36 +0800

    北京银行完成

--- dataFormatting.py
+++ dataFormatting.py
@@ -16,12 +16,18 @@ def format_progress(msg: str):
 # %%
 def format_bjyh(dir_path: pathlib.Path):
     format_progress('开始分析北京银行账户……')
+
     if dir_path.is_file():
         raise Exception('北京银行账户应该是一个文件夹！')
-    format_progress(doc)
-    accounts = pd.read_excel(dir_path / '北京银行账户情况.xlsx', header=1)
+
+    acc_file = next(dir_path.glob('*账户*.*'))
+    format_progress(acc_file.name)
+    accounts = pd.read_excel(acc_file, header=1)
+
+    trans_file = next(dir_path.glob('*交易明细*.*'))
+    format_progress(trans_file.name)
     tmp_trans_list = pd.read_excel(
-        dir_path / '交易明细模板-罗扬等（京行天分）.xls',
+        trans_file,
         sheet_name=None,
         converters={
             '帐号': str,
@@ -31,6 +37,7 @@ def format_bjyh(dir_path: pathlib.Path):
     for name in tmp_trans_list:
         tmp_trans_list[name]['户名'] = name
     transactions = pd.concat(tmp_trans_list.values())
+
     accounts['银行名称'] = "北京银行"
     transactions['银行名称'] = "北京银行"
     tmp_t = transactions['资金收付标志']
@@ -39,7 +46,7 @@ def format_bjyh(dir_path: pathlib.Path):
         '户名', '银行名称', '开户银行机构名称', '帐号', '交易日期', '交易方式', '资金收付标志', '金额', '余额',
         '户名', '交易对手姓名', '交易对手帐号', '交易对手金融机构名称'
     ]]
-    format_progress('end 北京银行')
+
     return accounts, transactions
 
 
